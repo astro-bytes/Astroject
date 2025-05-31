b01ae7f2daf0de277f4fb25556129e8fd836e613
Porter McGary <56183563+mcgaryp@users.noreply.github.com>
2025-05-30 18:23:55 -0600

    Update GraphTests.swift

--- Tests/CoreTests/GraphTests.swift
+++ Tests/CoreTests/GraphTests.swift
@@ -112,7 +112,7 @@ struct GraphTests {
     func threadSafety() async {
         let graph = Graph<Int>()
         var contexts = [Context]()
-        await withTaskGroup { group in
+        await withTaskGroup(of: Void.self) { group in
             for index in 0..<500 {
                 let context = Context.fresh()
                 contexts.append(context)
@@ -122,7 +122,7 @@ struct GraphTests {
             }
         }
         
-        await withTaskGroup { group in
+        await withTaskGroup(of: Void.self) { group in
             for index in 0..<500 {
                 let context = contexts[index]
                 group.addTask {
